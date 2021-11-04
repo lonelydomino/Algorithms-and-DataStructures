d59f38351d1f74219dc215645bb3a7cbec390cf5
Milo <zeroeclipse@live.com>
2021-11-03 19:19:14 -0700

    Testing performance.

--- practice.js
+++ practice.js
@@ -1,12 +1,17 @@
 
 
 const nemo = ["nemo"]
+const everyone = ["dory", "nemo", "gill","marlin"]
+const largeArray = new Array(1000).fill('nemo')
 
 function findNemo(array) {
+    let t0 = performance.now()
     for(let i = 0; i < array.length; i++){
         if(array[i] === 'nemo'){
             console.log("nemo found!")
         }
     }
+    let t1 = performance.now()
+    console.log("call to find nemo took " + (t1 - t0) + " milliseconds")
 }
-findNemo(nemo)
+findNemo(largeArray)

6216cb355b8d606719d3e5d96cdad15ae2291ddd
Milo <zeroeclipse@live.com>
2021-11-03 19:13:56 -0700

    First practice commit.

--- practice.js
+++ practice.js
@@ -1,1 +1,12 @@
-console.log("abc")
+
+
+const nemo = ["nemo"]
+
+function findNemo(array) {
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === 'nemo'){
+            console.log("nemo found!")
+        }
+    }
+}
+findNemo(nemo)

b8d7b38ce7981992fe2338dbc53e8573aefbb29f
Milo <zeroeclipse@live.com>
2021-11-03 20:12:52 -0700

    Adding Big O exercise 1.

--- practice.js
+++ practice.js
@@ -17,7 +17,36 @@ function findNemo(array) {
 findNemo(largeArray)
 
 
-//O(n) = Linear time
+//O(n) - Linear Time
 const compressAllBoxes = boxes => {
     boxes.forEach(box => console.log(box))
 }
+
+//O(1) - Constant Time
+
+const compressFirstBox = (boxes) => {
+    console.log(boxes[0])
+}
+
+//O(2)
+const compressFirstBox = (boxes) => {
+    console.log(boxes[0])
+    console.log(boxes[1])
+
+}
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function funChallenge(input) {
+    let a = 10;//O(1)
+    a = 50 + 3;//O(1)
+  
+    for (let i = 0; i < input.length; i++) {//O(n)
+      anotherFunction();//O(n)
+      let stranger = true;//O(n)
+      a++;//O(n)
+    }
+    return a;//O(1)
+  }
+
+  // 3 + n + n + n + n
+  // O (3+4n)

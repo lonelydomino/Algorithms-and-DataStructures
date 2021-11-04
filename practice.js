962e9ec207effa5e1a84b61ae40c776235ed3e54
Milo <zeroeclipse@live.com>
2021-11-03 20:51:57 -0700

    Adding four rules and quadratic time.

--- practice.js
+++ practice.js
@@ -50,3 +50,41 @@ function funChallenge(input) {
 
   // 3 + n + n + n + n
   // O (3+4n)
+
+
+
+  // What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+    let a = 5;//O(1)
+    let b = 10;//O(1)
+    let c = 50;//O(1)
+    for (let i = 0; i < input; i++) {//O(n)
+      let x = i + 1;//O(n)
+      let y = i + 2;//O(n)
+      let z = i + 3;//O(n)
+  
+    }
+    for (let j = 0; j < input; j++) {//O(n)
+      let p = j * 2;//O(n)
+      let q = j * 2;//O(n)
+    }
+    let whoAmI = "I don't know";//O(1)
+  }
+
+  // O(4 + 7n)
+  //or just O(n)
+
+  //1. Take the worst case
+  //2. Drop constants: O(n +1) -> O(n)
+  //3. Different Terms for inputs: function(a, b) = O(a + b)
+  //4. Drop Non Dominats
+
+//O(n^2) - Quadratic Time
+  const logAllPairsOfBoxes = (boxes) => {
+    boxes.forEach(function(firstBox){
+        boxes.forEach(function(secondBox){
+            console.log(firstBox, secondBox)
+        })
+    })
+
+  }

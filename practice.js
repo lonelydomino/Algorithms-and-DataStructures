c94f60dcbf6a21c9fa22ca8a0987e729b4ea7d5d
Milo <zeroeclipse@live.com>
2022-01-04 19:46:50 -0800

    Adding sorting arrays.

--- practice.js
+++ practice.js
@@ -77,7 +77,7 @@ function anotherFunChallenge(input) {
   //1. Take the worst case
   //2. Drop constants: O(n +1) -> O(n)
   //3. Different Terms for inputs: function(a, b) = O(a + b)
-  //4. Drop Non Dominats
+  //4. Drop Non Dominan1ts
 
 //O(n^2) - Quadratic Time
   const logAllPairsOfBoxes = (boxes) => {
@@ -99,5 +99,38 @@ function anotherFunChallenge(input) {
 
   array.unshift('x')//O(n)
 
-  array.splice(2, 0 , 'alien')//m
+  array.splice(2, 0 , 'alien')//O(n)z
 
+//Combine two sorted arrays
+
+function mergeSortedArrays(array1, array2){
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+  
+  //We should actually move these 2 if statements to line 2 so that we do the checks before we do assignments in line 3 and 4!
+  if(array1.length === 0) {
+    return array2;
+  }
+  if(array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item){
+   if(array2Item === undefined || array1Item < array2Item){
+     mergedArray.push(array1Item);
+     array1Item = array1[i];
+     i++;
+   }   
+   else {
+     mergedArray.push(array2Item);
+     array2Item = array2[j];
+     j++;
+   }
+  }
+  return mergedArray;
+}
+
+mergeSortedArrays([0,3,4,31], [3,4,6,30]);
